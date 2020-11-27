b637802b66ffca10a074cd5c227860ee6e87eda2
Sean Magaud <magaud.sean@gmail.com>
2020-11-27 15:22:51 +0100

    feat(theme): add theme switch mode

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { PageTitle } from '../../components'
 import { setPageTitle } from '../../utils/setPageTitle'
 import { useTranslation } from 'react-i18next'
+import { useThemeSwitcher } from '../../contexts/themeSwitcher'
+import { Button } from '../../components/Button'
 
 export default function Home() {
   const { t } = useTranslation()
+  const { switchTheme } = useThemeSwitcher()
 
   React.useEffect(() => {
     setPageTitle(t('home'))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return <PageTitle>{t('home')}</PageTitle>
+  return (
+    <>
+      <PageTitle>{t('home')}</PageTitle>
+      <Button onClick={switchTheme}>switch theme</Button>
+    </>
+  )
 }
