dfcec0609b71ddce68016fd88b372f3c91409531
Le Hen Diego <diego.lehen@ynov.com>
2020-11-27 13:51:24 +0100

    feat(PageContact): construction of the contact page

---
+++ src/pages/Contact/Contact.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { PageTitle } from '../../components/PageTitle'
+import { motion } from 'framer-motion'
+
+export const DivContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 15px;
+`
+
+export const FormContact = styled.div`
+  width: 70%;
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+`
+
+export const Title = styled(PageTitle)`
+  align-self: center;
+`
+export const MessageConfirm = styled(motion.p)`
+  align-self: center;
+  padding: 12px;
+  border: 1px solid ${({ theme }) => theme.colors.black.base};
+  margin-top: 12px;
+  opacity: 0;
+`
