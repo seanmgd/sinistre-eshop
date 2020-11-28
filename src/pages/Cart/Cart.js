d3bca41310ca8639af92f630e9456b2c65d31fdc
Sean Mgd <magaud.sean@gmail.com>
2020-11-28 19:52:39 +0100

    feat(page): add front only cart page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { PageTitle } from '../../components'
 import { setPageTitle } from '../../utils/setPageTitle'
+import {
+  Container,
+  Content,
+  Grid,
+  Overview,
+  InfoOverwiew,
+  ActionOverview,
+} from './Cart.styles'
+import { Button } from '../../components/Button'
+import { PageTitle } from '../../components/PageTitle'
 
 export default function Cart() {
   const { t } = useTranslation()
@@ -11,5 +20,56 @@ export default function Cart() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return <PageTitle>{t('cart')}</PageTitle>
+  return (
+    <Container>
+      <PageTitle>{t('cart')}</PageTitle>
+      <Content>
+        <Grid>
+          <ul>
+            <li>Photo</li>
+            <li>Hoodie</li>
+            <li>24 €</li>
+            <li>{t('qty')}: 4</li>
+            <li>Total: 96 €</li>
+            <ActionOverview>
+              <Button size="large">+</Button>
+              <Button size="large" color="error">
+                -
+              </Button>
+            </ActionOverview>
+          </ul>
+          <ul>
+            <li>Photo</li>
+            <li>Tees Long Sleeves</li>
+            <li>24 €</li>
+            <li>{t('qty')}: 4</li>
+            <li>Total: 96 €</li>
+            <ActionOverview>
+              <Button size="large">+</Button>
+              <Button size="large" color="error">
+                -
+              </Button>
+            </ActionOverview>
+          </ul>
+        </Grid>
+        <Overview>
+          <InfoOverwiew>
+            <div>
+              Total {t('products')} <span> 4 </span>
+            </div>
+            <div>
+              Total {t('checkout')} <span>96 €</span>
+            </div>
+          </InfoOverwiew>
+          <hr />
+          <ActionOverview>
+            <Button size="x-large">{t('checkout')}</Button>
+            <Button size="x-large" color="grey">
+              {t('clear')}
+            </Button>
+          </ActionOverview>
+        </Overview>
+      </Content>
+    </Container>
+  )
 }
