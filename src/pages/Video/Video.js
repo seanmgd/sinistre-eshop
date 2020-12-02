afe3e89622f147afdc1b11852827b2524ee3479f
Sean Mgd <magaud.sean@gmail.com>
2020-12-02 02:42:25 +0100

    chore(video): review code style

--- src/pages/Video/Video.js
+++ src/pages/Video/Video.js
@@ -2,55 +2,30 @@ import React from 'react'
 import { setPageTitle } from '../../utils/setPageTitle'
 import { useVideo } from '../../services/videos/query'
 import styled from 'styled-components'
-import { PageTitle } from '../../components/PageTitle'
-import { devices } from '../../constants/devices'
 
 export default function Video({ videoSlug }) {
-  const { name, slug, link, image_url, created_at } = useVideo(videoSlug)
+  const { name, link } = useVideo(videoSlug)
 
   React.useEffect(() => {
     setPageTitle(name)
   }, [name])
 
   return (
-    <DivContainer>
-      <Title>{name}</Title>
-      <VideoStyle title={name} src={link} allow="fullscreen" />
-    </DivContainer>
+    <Container>
+      <Iframe title={name} src={link} allow="fullscreen" />
+    </Container>
   )
 }
 
-const DivContainer = styled.div`
-  margin-top: 16px;
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: center;
+  height: 93vh;
+  width: 100vw;
 `
-const Title = styled(PageTitle)`
-  align-self: center;
-`
-
-const VideoStyle = styled.iframe`
-  margin-top: 12px;
-  align-self: center;
-  @media ${devices.mobileS} {
-    width: 310px;
-    height: 300px;
-  }
-  @media ${devices.mobileM} {
-    width: 370px;
-    height: 300px;
-  }
-  @media ${devices.mobileL} {
-    width: 425px;
-    height: 300px;
-  }
-  @media ${devices.tablet} {
-    width: 700px;
-    height: 500px;
-  }
-  @media ${devices.laptop} {
-    width: 1000px;
-    height: 500px;
-  }
+const Iframe = styled.iframe`
+  width: inherit;
+  height: inherit;
 `
