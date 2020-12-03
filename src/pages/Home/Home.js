d6812e55d049d6f52d737ff94169f792d13b2aea
Tristan Ramirez <tristan.rami@gmail.com>
2020-12-03 11:37:54 +0100

    chore(home): clean code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,34 @@
 import React from 'react'
-import { setPageTitle } from '../../utils/setPageTitle'
 import { useTranslation } from 'react-i18next'
+import { Button } from '../../components'
 import { useThemeSwitcher } from '../../contexts/themeSwitcher'
-import { Button } from '../../components/Button'
-import { CustomContainer, Title } from './Home.styles'
+import { setPageTitle } from '../../utils/setPageTitle'
+import { HomeTitle, HomeWrapper } from './Home.styles'
 
 export default function Home() {
   const { t, i18n } = useTranslation()
   const { switchTheme } = useThemeSwitcher()
-  const [isEnglish, setIsEnglish] = React.useState(true)
-  const changeLanguage = () => {
-    setIsEnglish(!isEnglish)
-    i18n.changeLanguage(isEnglish ? 'en' : 'fr')
-  }
+  const [lang, setLang] = React.useState(i18n.language)
 
   React.useEffect(() => {
     setPageTitle(t('home'))
   }, [t])
 
+  const handleSwitchLang = () => {
+    const newLang = lang.toLowerCase().includes('fr') ? 'en' : 'fr'
+    setLang(newLang)
+    i18n.changeLanguage(newLang)
+  }
+
   return (
-    <CustomContainer>
-      <Title>{t('welcome')}</Title>
+    <HomeWrapper>
+      <HomeTitle>{t('welcome')}</HomeTitle>
+
       <div>
         <Button onClick={switchTheme}>{t('switch_theme')}</Button>
-        <Button
-          onClick={() => {
-            changeLanguage()
-          }}
-        >
-          {t('switch_lang')}
-        </Button>
+
+        <Button onClick={handleSwitchLang}>{t('switch_lang')}</Button>
       </div>
-    </CustomContainer>
+    </HomeWrapper>
   )
 }
