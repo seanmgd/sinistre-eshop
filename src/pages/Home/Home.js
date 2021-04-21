a29fb5bcfc039946a83131811bf9c715960c75a1
Sean Magaud <sean.magaud@easy.movie>
2021-04-22 00:15:41 +0200

    maintenance mode

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button, ToastNotification, Newsletter } from '../../components'
+import { Button, Newsletter } from '../../components'
 import { useThemeSwitcher } from '../../contexts/themeSwitcher'
 import { setPageTitle } from '../../utils/setPageTitle'
 import { HomeTitle, HomeWrapper, ButtonContainer } from './Home.styles'
-import { sendNotification } from '../../services/firebase/query'
 import { stopEvent } from '@ttrmz/react-utils'
 
 export default function Home() {
   const { t, i18n } = useTranslation()
   const { switchTheme } = useThemeSwitcher()
   const [lang, setLang] = React.useState(i18n.language)
-  const [showToast, setShowToast] = React.useState(false)
 
   React.useEffect(() => {
     setPageTitle(t('home'))
@@ -22,12 +20,6 @@ export default function Home() {
     const newLang = lang.toLowerCase().includes('fr') ? 'en' : 'fr'
     setLang(newLang)
     i18n.changeLanguage(newLang)
-  }
-
-  const handleNotification = event => {
-    stopEvent(event)
-    setShowToast(!showToast)
-    sendNotification()
   }
 
   return (
@@ -44,19 +36,7 @@ export default function Home() {
         <Button onClick={handleSwitchLang} size="large" hover={true}>
           {t('switch_lang')}
         </Button>
-
-        <Button onClick={handleNotification} size="large" hover={true}>
-          {t('send_notification')}
-        </Button>
       </ButtonContainer>
-      {showToast && (
-        <div>
-          <ToastNotification
-            textNotification="Tu peux checker dans network que le call api se fait bien, mais la notif ne s'envoie pas..
-        Du coup préviens moi sur discord, donne moi le token qui est dans la console et je te trigger ça"
-          />
-        </div>
-      )}
     </HomeWrapper>
   )
 }
