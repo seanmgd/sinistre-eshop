94dcbfec4341d418444572883e29dc5c8efeee09
Sean Magaud <sean.magaud@easy.movie>
2021-04-22 12:48:04 +0200

    fix deploy

--- src/app/App.js
+++ src/app/App.js
@@ -1,31 +1,31 @@
-import { Router } from '@reach/router'
+// import { Router } from '@reach/router'
 import React from 'react'
-import { useUserContext } from '../contexts/user'
-import styled from 'styled-components'
-const Layout = React.lazy(() => import('../layout'))
-const Home = React.lazy(() => import('../pages/Home/Home'))
-const Dashboard = React.lazy(() => import('../pages/Dashboard/Dashboard'))
-const Account = React.lazy(() => import('../pages/Account/Account'))
-const Artists = React.lazy(() => import('../pages/Artists/Artists'))
-const Artist = React.lazy(() => import('../pages/Artist/Artist'))
-const Blog = React.lazy(() => import('../pages/Blog/Blog'))
-const Cart = React.lazy(() => import('../pages/Cart/Cart'))
-const Checkout = React.lazy(() => import('../pages/Checkout/Checkout'))
-const Contact = React.lazy(() => import('../pages/Contact/Contact'))
-const Login = React.lazy(() => import('../pages/Login/Login'))
-const Products = React.lazy(() => import('../pages/Products/Products'))
-const Product = React.lazy(() => import('../pages/Product/Product'))
-const Videos = React.lazy(() => import('../pages/Videos/Videos'))
-const Video = React.lazy(() => import('../pages/Video/Video'))
-const Logout = React.lazy(() => import('../pages/Logout/Logout'))
+// import { useUserContext } from '../contexts/user'
+// import styled from 'styled-components'
+// const Layout = React.lazy(() => import('../layout'))
+// const Home = React.lazy(() => import('../pages/Home/Home'))
+// const Dashboard = React.lazy(() => import('../pages/Dashboard/Dashboard'))
+// const Account = React.lazy(() => import('../pages/Account/Account'))
+// const Artists = React.lazy(() => import('../pages/Artists/Artists'))
+// const Artist = React.lazy(() => import('../pages/Artist/Artist'))
+// const Blog = React.lazy(() => import('../pages/Blog/Blog'))
+// const Cart = React.lazy(() => import('../pages/Cart/Cart'))
+// const Checkout = React.lazy(() => import('../pages/Checkout/Checkout'))
+// const Contact = React.lazy(() => import('../pages/Contact/Contact'))
+// const Login = React.lazy(() => import('../pages/Login/Login'))
+// const Products = React.lazy(() => import('../pages/Products/Products'))
+// const Product = React.lazy(() => import('../pages/Product/Product'))
+// const Videos = React.lazy(() => import('../pages/Videos/Videos'))
+// const Video = React.lazy(() => import('../pages/Video/Video'))
+// const Logout = React.lazy(() => import('../pages/Logout/Logout'))
 
-const PageWrapper = styled.div`
-  width: 100vw;
-`
+// const PageWrapper = styled.div`
+//   width: 100vw;
+// `
 
 export function App() {
-  const { user } = useUserContext()
-  const isAuth = user.token
+  // const { user } = useUserContext()
+  // const isAuth = user.token
 
   return (
     <div
